44a62fcaee2b450850fc6a7edd831c7aad71b03a
another19 <trinhbhuy19@gmail.com>
2017-08-09 23:41:17 +0700

    ok

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,21 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>no title</title>
 
-
+    <link rel="stylesheet" href="css/my.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
+    <style media="screen">
+        body {
+            background: url("https://www.foxcomputers.nl/wp-content/uploads/foxwall.jpg") no-repeat ;
+            background-size: cover;
+        }
+    </style>
 
   </head>
-  <body style="background-image: url('img/bglogin.jpg')">
+  <body >
+    <div class="form">
       <form class="" action="index.html" method="post">
           <div class="form-group">
-            <label for=""></label>
-            <input type="text" class="form-control" id="" placeholder="">
-            <p class="help-block">Help text here.</p>
+            <label for="">User Name</label>
+            <input type="text" class="form-control" id="" placeholder="user">
           </div>
+          <div class="form-group">
+            <label for="">Password</label>
+            <input type="text" class="form-control" id="" placeholder="password">
+          </div>
+          <input type="submit" >
       </form>
-
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   </body>
